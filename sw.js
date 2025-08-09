d4cfc7bd46745d7966303ebcd3fe6d4cf6c675ec
lunarossa-apps <christian.maglia@gmail.com>
2025-08-09 15:03:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='tc-v7-7-1-core';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css?v=7.7.1','./app.js?v=7.7.1','./manifest.json'])))});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim()); e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE='tc-v7-7-3-core';
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+    './','./index.html','./style.css?v=7.7.3','./app.js?v=7.7.3','./manifest.json'
+  ])))
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(self.clients.claim());
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
